c60c58df6910dca4c812f1dd5de8b111a717ae6f
Yang2310 <J.GUO7@universityofgalway.ie>
2025-05-27 20:32:56 +0800

    1

--- data.py
+++ data.py
@@ -1,1 +1,19 @@
 import pandas as pd
+import os
+
+# Loading reference relationships
+cit_df = pd.read_csv(
+    "data/cit-HepTh.txt",
+    sep="\t",
+    header=None,
+    names=["source", "target"]
+)
+
+# Loading submission time
+dates_df = pd.read_csv(
+    "data/cit-HepTh-dates.txt",
+    sep="\t",
+    header=None,
+    names=["paper_id", "date"]
+)
+dates_df["year"] = pd.to_datetime(dates_df["date"]).dt.year
